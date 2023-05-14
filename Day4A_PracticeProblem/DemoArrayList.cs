4afe6c45feee72892a4b28e6d4fc95874057ac9f
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-14 18:37:33 +0530

    Add: Program for arraylist practice

---
+++ Day4A_PracticeProblem/DemoArrayList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4A_PracticeProblem
+{
+    public class DemoArrayList
+    {
+        public void MethodArrayList()
+        {
+            ArrayList list = new ArrayList();
+            // Create 
+            list.Add("apple");
+            list.Add("banana");
+            list.Add("cherry");
+            list.Add("blueberry");
+            list.Add("guava");
+            list.Add("mango");
+            // Read 
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            // Update 
+            Console.WriteLine("*******Update in list********");
+            list[1] = "orange";
+            list[2] = "pineapple";
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            // Delete 
+            Console.WriteLine("********Delete in list********");
+            list.Remove("guava");
+            list.Remove("mango");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Day4A_PracticeProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4A_PracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoArrayList demoArrayList = new DemoArrayList();
+            demoArrayList.MethodArrayList();
+            Console.ReadLine();
+        }
+    }
+}
